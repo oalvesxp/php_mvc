2ab6c5e5d9715d6a9d3f40fcf2d10a514436b109
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-06-16 15:01:04 -0300

    Aula 5.2

--- src/Controller/Error404Controller.php
+++ src/Controller/Error404Controller.php
@@ -6,39 +6,6 @@ class Error404Controller implements Controller
 {
     public function processRequest(): void
     {
-        require_once __DIR__ . '/../../_header.php'; ?>
-            <style>
-                .container__error {
-                    margin: 3em 0;
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    justify-content: center;
-                    text-align: initial;
-                }
-
-                .error__titulo {
-                    color: var(--azul-medio);
-                    font-weight: bold;
-                    font-size: 32px;
-                    margin-bottom: 20px;
-                }
-
-                .error__mensagem {
-                    color: var(--azul-escuro);
-                    font-weight: regular;
-                    font-size: 24px;
-                }
-
-            </style>
-
-            <main class="container">
-                <div class="container__error">
-                    <h2 class="error__titulo">Error 404</h2>      
-                    <p class="error__mensagem">Desculpe... Não foi possível encontrar "<?= $_SERVER['PATH_INFO']?>".</p>
-                </div>
-            </main>
-        <?php require_once __DIR__ . '/../../_footer.php';
-
+        require_once __DIR__ . '/../../views/error-404.php';
     }
 }
