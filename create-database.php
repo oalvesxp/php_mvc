e45f50eb55fe567ca69642160c6bf543575a64e9
Osvaldo A. Neto <oalvesxp@gmail.com>
2024-07-04 15:44:29 -0300

    Aula 1.3

--- create-database.php
+++ create-database.php
@@ -3,11 +3,21 @@
 $dbPath = __DIR__ . '/database.sqlite';
 $pdo = new PDO("sqlite:$dbPath");
 
-$pdo->exec("
+/** Tabela de videos */
+/**$pdo->exec("
     CREATE TABLE VID010
         (
             VID_ID INTEGER PRIMARY KEY
             , VID_URL VARCHAR
             , VID_TITLE VARCHAR
+        );
+");*/
+
+$pdo->exec("
+    CREATE TABLE USR010
+        (
+            USR_ID INTEGER PRIMARY KEY
+            , USR_EMAIL VARCHAR
+            , USR_PASSWD VARCHAR
         );
 ");
