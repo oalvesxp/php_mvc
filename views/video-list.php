af67790fd714fc642b3d2ed2e7ad8ed26e512048
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-07-05 22:25:12 -0300

    Aula 3.8

--- views/video-list.php
+++ views/video-list.php
@@ -5,10 +5,16 @@
         <?php foreach ($videos as $video): ?>
             <?php if (str_starts_with($video->url, 'http')): ?>
                 <li class="videos__item">
-                    <iframe width="100%" height="72%" src="<?= $video->url; ?>"
-                        title="YouTube video player" frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen></iframe>
+                    <?php if ($video->getFilePath() !== null): ?>
+                        <a href="<?= $video->url; ?>">
+                            <img src="/img/uploads/<?= $video->getFilePath(); ?>" alt="" style="width: 100%;" />
+                        </a>
+                    <?php else: ?>
+                        <iframe width="100%" height="72%" src="<?= $video->url; ?>"
+                            title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                    <?php endif; ?>
                     <div class="descricao-video">
                         <img src="./img/logo.png" alt="logo canal alura">
                         <h3><?= $video->title; ?></h3>
